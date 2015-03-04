fc4c14033868429d99987a987c6b62bbc23ee11b
HerrR <ribe93@live.se>
2015-03-04 15:18:30 +0100

    Getting started

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -3,20 +3,104 @@
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
-dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
+dinnerPlannerApp.factory('Dinner',function ($resource, $cookieStore) {
+  var pending = "none";
   var numberOfGuest = 2;
+  var menu = [];
+  var dishes = [];
+  var loading = true;
+  var error = false;
 
+  this.isLoading = function(){
+    return loading;
+  }
+
+  this.hasError = function(){
+    return error;
+  }
 
   this.setNumberOfGuests = function(num) {
     numberOfGuest = num;
+    $cookieStore.put("numGuests", numberOfGuest);
   }
 
   this.getNumberOfGuests = function() {
+    var numberOfGuest = cookieStore.get("numGuests");
     return numberOfGuest;
   }
 
+  this.getDish = $resource('http://api.bigoven.com/recipe/:id',{api_key:'dvxLl271adHi9kSJNj29sNWp256I35Y0'});
+  this.searchDishes = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:25,api_key:'dvxLl271adHi9kSJNj29sNWp256I35Y0'});
 
+  this.addPending = function(id){
+    pending = getDish.get({id:id});
+  }
+
+  this.getPending = function(){
+    return pending;
+  }
+
+  this.removePending = function(){
+    pending = "none";
+  }
+
+  this.getFullMenu = function() {
+    return menu;
+  }
+
+  this.getTotalMenuPrice = function() {
+    var totalMenuPrice = 0;
+    for(dish in menu) {
+      totalMenuPrice += this.getNumberOfGuests()*menu[dish].pricePerPerson;
+    }
+    return totalMenuPrice;
+  }
+
+  this.addDishToMenu = function(pending) {
+    var duplicates = false;
+    if(menu.length === 0){
+      menu.push(pending);
+    } else {
+      for(dish in menu){
+        if(menu[dish].RecipeID === pending.RecipeID){
+          alert(menu[dish].Title +" is already in the menu!");
+          duplicates = true;
+          break;
+        }
+      }
+      if(!duplicates){
+        if(menu.length<3){
+          menu.push(pending);
+        } else {
+          menu.splice(2,1,pending);
+        }
+      }
+    }
+  }
+
+  this.removeDishFromMenu = function(id) {
+    for(dish in menu){
+      if(menu[dish].RecipeID === id){
+        menu.splice(dish, 1);
+      }
+    }
+  }
+
+  this.generateDishes = function(type, filter){
+    dishes = searchDishes.get();
+  }
+
+  this.getPriceOfDish = function(dish){
+    var priceOfDish = 0;
+    for(ingredient in dish.Ingredients){
+      priceOfDish += dish.Ingredients[ingredient].Quantity * guests;
+    }
+    return priceOfDish;
+  }
+
+  this.getAllDishes = function() {
+    return dishes;
+  }
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
   // you will need to modify the model (getDish and getAllDishes) 
